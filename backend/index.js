79cb50f73d598caf23a32bc6717b06d8d12d861f
rajprabhe <rajprabhe@gmail.com>
2026-08-01 22:08:43 +0530

    add path module

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const path = require("path");  
 dotenv.config();
 connectDB();
 
