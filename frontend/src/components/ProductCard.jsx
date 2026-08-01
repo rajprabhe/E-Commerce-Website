7826f46546351a5aae9904ea7bd017d995e7f58a
rajprabhe <rajprabhe@gmail.com>
2026-08-01 20:32:39 +0530

    fixed rupees value

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({product}) => {
         <img src={product.imageUrl} alt={product.name} className='product-image' />
         <div className='product-info'>
             <h3 className='product-name'>{product.name}</h3>
-            <p className='product-price'>${product.price.toFixed(2)}</p>
+            <p className='product-price'>₹ {product.price.toFixed(2)}</p>
             <Link to={`/product/${product._id}`} className='view-details-button'>
                view Details
             </Link>
